6b3678ddd091490799ddc1fa64fcb43cc191861f
beatricerocha <beatricerocha.it@gmail.com>
2022-03-20 22:27:44 -0300

    Aula 3 17.03

---
+++ aula-03/ChamaClasses/src/ChamaMetodo.java
@@ -0,0 +1,14 @@
+
+public class ChamaMetodo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Esta linha permite chamar a classe desejada
+		Resposta texto = new Resposta();
+		
+		// Este processo abre a classe e envoca o método desenvolvido
+		texto.exibeResposta();
+	}
+
+}
---
+++ aula-03/ChamaClasses/src/Resposta.java
@@ -0,0 +1,8 @@
+
+public class Resposta {
+// criando a publicação para a chamada deste método
+	public void exibeResposta() {
+		System.out.println("Este é o método de resposta");
+	}
+	
+}
